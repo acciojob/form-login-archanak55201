fe2b316474f9118689ba7df8c40f1db198791ac2
archanak55201 <55021100+archanak55201@users.noreply.github.com>
2023-07-23 17:27:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const button=document.querySelector("input[name=submit]");
+// const button=document.querySelector("input[name=submit]");
 
 function getFormvalue() {
     const fname=document.getElementsByTagName("input");
    
-	alert(fname[0].value+" "+fname[1].value);
+	window.alert(fname[0].value+" "+fname[1].value);
 
 } 
 
 
-button.addEventListener("click",getFormvalue());
+// button.addEventListener("submit",getFormvalue());
